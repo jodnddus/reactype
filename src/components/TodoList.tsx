eb71f9cc0008182e0fe6648ebc60f0e5e228febe
조웅연 <lockhost23@gmail.com>
2019-06-27 20:58:44 +0900

    onSubmit, onRemove, onChange 함수 추가

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -26,15 +26,40 @@ class TodoList extends React.Component<Props, State> {
 
   onToggle = (id: number): void => {
     const { todoItems } = this.state;
-    const nextTodoItems:TodoItemState[] = todoItems.map(item => {
+    const nextTodoItems: TodoItemState[] = todoItems.map(item => {
       if (item.id === id) {
         item.done = !item.done;
       }
       return item;
     });
-    
+
     this.setState({
       todoItems: nextTodoItems
+    });
+  }
+
+  onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const { todoItems, input } = this.state;
+    const newItem: TodoItemState = { id: this.nextTodoId++, text: input, done: false};
+    const nextTodoItems: TodoItemState[] = todoItems.concat(newItem);
+    this.setState({
+      todoItems: nextTodoItems
+    });
+  }
+
+  onRemove = (id: number): void => {
+    const { todoItems } = this.state;
+    const nextTodoItems: TodoItemState[] = todoItems.filter(item => item.id !== id);
+    this.setState({
+      todoItems: nextTodoItems
+    });
+  }
+
+  onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = e.currentTarget;
+    this.setState({
+      input: value
     });
   }
 }
