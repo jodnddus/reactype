6db58f54c3fac1e67a7b5bd57be4bc4978c5de9e
조웅연 <lockhost23@gmail.com>
2019-06-28 09:09:54 +0900

    action 추가

--- src/store/modules/todos.ts
+++ src/store/modules/todos.ts
@@ -43,3 +43,51 @@ interface ChangeInputAction {
 }
 
 export type TodoActionTypes = | CreateAction | RemoveAction | ToggleAction | ChangeInputAction;
+
+// actions
+let autoId = 0;
+
+function create(text: string) {
+  return {
+    type: CREATE,
+    payload: {
+      id: autoId++,
+      text: text,
+      done: false
+    }
+  };
+}
+
+function remove(id: number) {
+  return {
+    type: REMOVE,
+    meta: {
+      id
+    }
+  };
+}
+
+function toggle(id: number) {
+  return {
+    type: TOGGLE,
+    meta: {
+      id
+    }
+  };
+}
+
+function changeInput(input: string) {
+  return {
+    type: CHANGE_INPUT,
+    meta: {
+      input
+    }
+  };
+}
+
+export const actionCreators = {
+  create,
+  remove,
+  toggle,
+  changeInput
+};
