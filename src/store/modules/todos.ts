60ce2d5e9db33ab8a34f1d0cf6d62e1be97799e7
조웅연 <lockhost23@gmail.com>
2019-06-28 09:04:37 +0900

    redux에서 사용할 타입 정의

---
+++ src/store/modules/todos.ts
@@ -0,0 +1,45 @@
+// types
+
+export interface TodoItemDataParams {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+export interface TodoState {
+  todoItems: TodoItemDataParams[];
+  input: string;
+}
+
+export const CREATE = 'todo/CREATE';
+export const REMOVE = 'todo/REMOVE';
+export const TOGGLE = 'todo/TOGGLE';
+export const CHANGE_INPUT = 'todo/CHANGE_INPUT';
+
+interface CreateAction {
+  type: typeof CREATE;
+  payload: TodoItemDataParams;
+}
+
+interface RemoveAction {
+  type: typeof REMOVE;
+  meta: {
+    id: number; 
+  };
+}
+
+interface ToggleAction {
+  type: typeof TOGGLE;
+  meta: {
+    id: number;
+  };
+}
+
+interface ChangeInputAction {
+  type: typeof CHANGE_INPUT;
+  meta: {
+    input: string;
+  };
+}
+
+export type TodoActionTypes = | CreateAction | RemoveAction | ToggleAction | ChangeInputAction;
