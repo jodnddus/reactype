24f06b5de4367f8fe9aa1c3ec04b604fe550097b
조웅연 <lockhost23@gmail.com>
2019-06-28 09:29:28 +0900

    reducers 추가

--- src/store/modules/todos.ts
+++ src/store/modules/todos.ts
@@ -91,3 +91,42 @@ export const actionCreators = {
   toggle,
   changeInput
 };
+
+// reducers
+
+const initialState: TodoState = {
+  todoItems: [],
+  input: ''
+};
+
+export function todoReducer(state = initialState, action: TodoActionTypes): TodoState {
+  switch (action.type) {
+    case CREATE:
+      return {
+        input: "",
+        todoItems: [ ...state.todoItems, action.payload ]
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todoItems: state.todoItems.filter(todo => todo.id !== action.meta.id)
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todoItems: state.todoItems.map(todo => {
+          if (todo.id === action.meta.id) {
+            todo.done = !todo.done;
+          }
+          return todo;
+        })
+      };
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.meta.input
+      };
+    default:
+      return state
+  }
+}
